2821ee20f7ff3e07513fc6c65c02c0040870242a
new name <53913248+kpsr3636@users.noreply.github.com>
2024-06-20 18:21:28 +0530

    added some methods in repository

--- src/main/java/com/example/weather/demo/repository/WeatherRepository.java
+++ src/main/java/com/example/weather/demo/repository/WeatherRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 public interface WeatherRepository extends CrudRepository<Weather, Long> {
     void deleteByCity(String city);
     void deleteByAlert(String alert);
+    void deleteByAlertType(String alertType);
+    void deleteByAlertLevel(String alertLevel);
+    String findByCity(String city);
 }
 
